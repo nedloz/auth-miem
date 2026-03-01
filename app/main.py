c14a01d9db2928b7a136d2434d6f611e821157d3
pyroman17 <78970393+DrogBruh@users.noreply.github.com>
2026-03-01 19:22:56 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.routers import auth
-# from app.routers import profile # Раскомментируй когда напишешь профиль
+from app.routers import profile 
 
 app = FastAPI(
     title="Auth Service",
@@ -9,7 +9,7 @@ app = FastAPI(
 )
 
 app.include_router(auth.router, prefix="/api/v1/auth", tags=["Auth"])
-# app.include_router(profile.router, prefix="/api/v1/users", tags=["Profile"])
+app.include_router(profile.router, prefix="/api/v1/users", tags=["Profile"])
 
 @app.get("/health")
 async def health_check():
